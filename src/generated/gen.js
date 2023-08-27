9bd7bc6774f700d5261073d5afa14d30631fad0b
Marcel Wiessler <marcel@gaisterhand.de>
2023-08-27 18:09:11 +0200

    update config and checkin files

---
+++ src/generated/gen.js
@@ -0,0 +1,16 @@
+﻿import "./register_types.ts"
+
+export const needle_exported_files = new Array();
+globalThis["needle:codegen_files"] = needle_exported_files;
+needle_exported_files.push("assets/SvelteKitSample.glb?v=1693152417867");
+document.addEventListener("DOMContentLoaded", () =>
+{
+	const needleEngine = document.querySelector("needle-engine");
+	if(needleEngine && needleEngine.getAttribute("src") === null)
+	{
+		needleEngine.setAttribute("hash", "1693152417867");
+		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
+	}
+});
+
+console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 3\.11\.6");
