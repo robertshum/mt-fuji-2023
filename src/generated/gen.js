e76160748fd9bcc995c8f7f5e0098d36cc9f6ef8
Marcel Wiessler <marcel@gaisterhand.de>
2023-08-28 16:28:42 +0200

    Update assets

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,15 +2,15 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/SvelteKitSample.glb?v=1693152417867");
+needle_exported_files.push("assets/SvelteKitSample.glb?v=1693232900157");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1693152417867");
+		needleEngine.setAttribute("hash", "1693232900157");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
 
-console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 3\.11\.6");
+console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 3\.12\.0-pre\.2");
