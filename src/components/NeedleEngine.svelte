bcad044eb7a3f0af7e1c13b3efbdd0713a2515bd
Marcel Wiessler <marcel@gaisterhand.de>
2024-01-02 12:16:36 +0100

    Update Needle Engine and set `src` property from codegen

--- src/components/NeedleEngine.svelte
+++ src/components/NeedleEngine.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
     import { onMount } from "svelte";
 
+    $: src = "";
+
     onMount(async () => {
-        await import("../generated/gen.js");
         await import("@needle-tools/engine");
+        await import("../generated/gen.js");
+        // in case no src is defined we use the global variable that is set inside of the codegen file gen.js
+        src = globalThis["needle:codegen_files"] as any;
     });
 </script>
 
-<needle-engine loading-style="light" {...$$props} />
+<needle-engine loading-style="light" {src} {...$$props} />
 
 <style>
     needle-engine {
