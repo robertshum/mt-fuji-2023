46af77f18a667892d0ea24509c37326c2ec663e5
robertshum <robertshum@gmail.com>
2024-03-01 10:46:08 -0800

    test fix by removing sample bundle

--- src/generated/register_types.ts
+++ src/generated/register_types.ts
@@ -1,12 +1,10 @@
-﻿// Import types from dependencies
-import "needle.samples.postprocessing"
-import "needle.samples.postprocessing/codegen/register_types.ts"
-
-/* eslint-disable */
+﻿/* eslint-disable */
 import { TypeStore } from "@needle-tools/engine"
 
 // Import types
 import { ButtonEvent } from "../scripts/buttonEvent.js";
+import { FocusDistancer } from "../scripts/FocusDistancer.js";
 
 // Register types
 TypeStore.add("ButtonEvent", ButtonEvent);
+TypeStore.add("FocusDistancer", FocusDistancer);
